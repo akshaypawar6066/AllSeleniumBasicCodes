25a31865e7801e5ba72b95e076bf4957d491aede
Akshay <akshaypawar6066@gmail.com>
2023-03-04 20:30:21 +0530

    Added code for Downloading files in selenium

---
+++ src/main/java/demo/FileDownloadInSelenium.java
@@ -0,0 +1,40 @@
+package demo;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FileDownloadInSelenium {
+	private static String filePath = "C:\\Users\\aksha\\downloads\\";
+	private static String fileName = "Sample3.pdf";
+	private static String desiredFilePath = "C:\\ChromeDownloads";
+
+	public static void main(String[] args) throws InterruptedException {
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put("download.default_directory", desiredFilePath);  //Download file at desired location.
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", hm);
+		options.addArguments("--disable-notifications");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().deleteAllCookies();
+		driver.get("https://filesamples.com/formats/pdf");
+		System.out.println("Before download file exists:" + isFileExists(filePath + fileName));
+		driver.findElement(By.xpath("(//div[@id='output']//a)[1]")).click(); // Download file at default loaction.
+		Thread.sleep(5000);
+		System.out.println("After download file exists:" + isFileExists(filePath + fileName));
+
+	}
+
+	public static boolean isFileExists(String filePath) {
+		File file = new File(filePath);
+		boolean b = file.exists();
+		return b;
+	}
+}
